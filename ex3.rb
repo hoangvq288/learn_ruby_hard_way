f4cd40a577b503b6f15b073abd5459d4c20dc38d
Vu Quoc Hoang <hoangvq288@gmail.com>
2016-09-03 15:17:00 +0700

    Exercise 3

---
+++ ex3.rb
@@ -0,0 +1,23 @@
+puts "I will count my chickens:"
+# / is priority then +
+puts "Hens #{25 + 30 / 6}"
+# Write in float type
+puts "Hens #{25 + 30 / 6.0}"
+# * and % are priority then -
+puts "Roosters #{100 - 25 * 3 % 4}"
+puts "Roosters #{100 - 25 * 3.0 % 4}"
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7
+
+puts "Oh, that's why it's false."
+
+puts "how about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
